bb4d15ce53601ec92f6211fa5627b447c19c7946
Nazim Boudeffa <boudeffa@gmail.com>
2020-06-16 01:17:57 +0200

    Added express landing page

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 'use strict';
 const BootBot = require('bootbot');
-//const config = require('./config.json');
-const config = {"accessToken" : process.env.accessToken,"appSecret" : process.env.appSecret}
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/public'))
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+const config = require('./config.json');
+//const config = {"accessToken" : process.env.accessToken,"appSecret" : process.env.appSecret}
 
 const bot = new BootBot({
   accessToken: config.accessToken,
@@ -14,6 +29,11 @@ bot.on('message', (payload, chat) => {
   chat.say(`Echo: ${text}`);
 });
 
+let botport = 3001;
 let port = process.env.PORT || 3000;
 
-bot.start(port);
+bot.start(botport);
+
+app.listen(port, function () {
+  console.log('Work app listening on port 3000!')
+})
