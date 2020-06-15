d551ea82b3facc321cd5c40df6c84b45c26f0022
Nazim Boudeffa <boudeffa@gmail.com>
2020-06-16 01:41:03 +0200

    Problem with using a port for express

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 'use strict';
 const BootBot = require('bootbot');
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const path = require('path')
-
-// set the view engine to ejs
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/public'))
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.render('index')
-})
-
 //const config = require('./config.json');
 const config = {"accessToken" : process.env.accessToken,"appSecret" : process.env.appSecret}
 
@@ -29,11 +14,6 @@ bot.on('message', (payload, chat) => {
   chat.say(`Echo: ${text}`);
 });
 
-let botport = 3001;
 let port = process.env.PORT || 3000;
 
-bot.start(botport);
-
-app.listen(port, function () {
-  console.log('Work app listening on port 3000!')
-})
+bot.start(port);
