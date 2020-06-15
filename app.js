e24be9d627d40fecb2f60645abea08cb25262ef1
Nazim Boudeffa <boudeffa@gmail.com>
2020-06-16 00:44:57 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const BootBot = require('bootbot');
+//const config = require('./config.json');
+const config = {"accessToken" : process.env.accessToken,"appSecret" : process.env.appSecret}
+
+const bot = new BootBot({
+  accessToken: config.accessToken,
+  verifyToken: 'naz',
+  appSecret: config.appSecret
+});
+
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Echo: ${text}`);
+});
+
+bot.start();
